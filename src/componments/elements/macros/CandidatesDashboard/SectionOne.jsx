5636ce774fc85f1f6cbe9741225290bb6589d52f
thattallman <rachit1947saxena@gmail.com>
2024-06-21 11:41:57 +0530

    fixed all resizing issues

--- src/componments/elements/macros/CandidatesDashboard/SectionOne.jsx
+++ src/componments/elements/macros/CandidatesDashboard/SectionOne.jsx
@@ -9,12 +9,12 @@ const SectionOne = () => {
     <>
       <div className=" w-[762px] absolute  h-[762px] top-[-354px] left-[-356px] bg-custom-colour "></div>
       <div className="bg-[#341845]  h-screen md:h-[850px] w-screen md:grid md:grid-cols-5 px-5 ">
-        <div className="     hidden md:col-span-3 md:flex flex-col justify-center items-center gap-[15px] md:px-12 px-8 h-screen  md:h-[850px]  ">
-          <h1 className="font-poppins text-white  md:text-[47px] text-[33px] md:text-justify text-center">
+        <div className="     hidden md:col-span-3 md:flex flex-col justify-center items-center gap-[15px] md:px-12 px-8  md:h-[850px]  ">
+          <h1 className="font-poppins text-white  md:text-[47px] text-[33px] text-justify ">
             Exceptional openings available in the
             <span className="font-bold"> nuclear domain! </span>
           </h1>
-          <p className="font-poppins text-white  text-[18px] md:text-justify text-center">
+          <p className="font-poppins text-white  text-[18px] md:text-left text-center">
             Since 2019, we have been supporting individuals secure new positions
             in the nuclear sector, drawing on our unparalleled expertise in the
             field.
@@ -42,15 +42,19 @@ const SectionOne = () => {
         </div>
 
         <div className="col-span-2  hidden md:grid grid-cols-3">
+          
           <div className="flex flex-col justify-center  px-1">
             <img src={candidateLeft} alt="" />
           </div>
-          <div className="flex flex-col justify-end  px-1">
+
+          <div className="flex flex-col justify-end  px-1 py-10">
             <img src={candidateMiddle} alt="" />
           </div>
+
           <div className="flex flex-col justify-center  px-1">
             <img src={candidateRight} alt="" />
           </div>
+
         </div>
       </div>
     </>
