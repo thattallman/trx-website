2fb216376f45c080e7c50b182b06ca7a56d87049
thattallman <rachit1947saxena@gmail.com>
2024-06-26 10:44:47 +0530

    rollback to previous changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,18 @@
 import RenderRoutes from "./routes/RenderRoutes";
-import { BrowserRouter,Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Navbar from "./componments/headers/Navbar";
 import MobileHeader from "./componments/headers/MobileHeader";
-import HomePage from './pages/landing/Home'
-import  ClientPage from "./pages/landing/Client";
- import CandidatePage  from "./pages/landing/Candidates";
- 
-
 
 
 function App() {
   return (
-    <>
-  
-      {/* <RenderRoutes /> */}
-     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/client" element={<ClientPage />} />
-      <Route path="/candidates" element={<CandidatePage />} />
-      </Routes>
+    <BrowserRouter>
    
 
+      <RenderRoutes />
       <Navbar />
-      </>
-   
- 
+      <MobileHeader />
+    </BrowserRouter>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        {/* Define your routes here */}
-        <Route path="/" element={<App />} />
-        {/* Add a route for unmatched paths */}
-        <Route path="*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
 )
