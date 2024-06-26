df20ce4ab1925f014198291f8508591dd8266979
thattallman <rachit1947saxena@gmail.com>
2024-06-26 10:36:54 +0530

    trying to fix router issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        {/* Define your routes here */}
+        <Route path="/" element={<App />} />
+        {/* Add a route for unmatched paths */}
+        <Route path="*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
